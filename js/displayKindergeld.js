970c27ae8efedf4112a2cf79a37c11f593a8a1a0
Amedee Van Gasse 🦄🌈✨ (they/them) <amedee-github@amedee.be>
2021-11-12 20:44:27 +0100

    💬 Format output string

--- js/displayKindergeld.js
+++ js/displayKindergeld.js
@@ -9,10 +9,22 @@ function displayKindergeld () { // eslint-disable-line require-jsdoc
 
     let aantalKinderen = eKinderen.value;
     let maandloon = eMaandloon.value;
-    let outputText = 'foo';
+    let outputText = '';
 
     if (aantalKinderen <= 0 || maandloon <= 0) {
         alert('Aantal kinderen en/of maandloon moeten meer dan 0 zijn'); // eslint-disable-line no-alert
+    } else {
+        let kindergeld = 0;
+        let kindString = 'kind';
+        if (aantalKinderen > 1) {
+            kindString = 'kinderen';
+        }
+
+        outputText = `
+            Met een maandloon van ${maandloon}
+            en ${aantalKinderen} ${kindString},
+            heeft deze moeder recht op €${kindergeld} kindergeld.
+        `
     }
     eOutput.innerHTML = outputText;
 }
