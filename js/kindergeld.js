ee7b0b34a943dae0b876caf7786b3e2acfc68650
Amedee Van Gasse 🦄🌈✨ (they/them) <amedee-github@amedee.be>
2021-11-12 11:36:41 +0100

    ♻️ Extract defaultKindergeld
    Don't want Magic Numbers.

--- js/kindergeld.js
+++ js/kindergeld.js
@@ -1,10 +1,12 @@
+const defaultKindergeld = 25;
+
 /**
  * @param {number} aantalKinderen Hoeveel kinderen heeft de moeder ten laste
  * @param {number} maandloon Wat is het maandloon van de moeder
  * @returns {number} Het kindergeld waar de moeder recht op heeft
  */
 function berekenKindergeld (aantalKinderen, maandloon) { // eslint-disable-line no-unused-vars
-    return 25;
+    return defaultKindergeld;
 }
 
 module.exports = {berekenKindergeld}
