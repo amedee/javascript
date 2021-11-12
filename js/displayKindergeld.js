fefa6604e84ee1a7f82bb15c089663d3aa3569fa
Amedee Van Gasse 🦄🌈✨ (they/them) <amedee-github@amedee.be>
2021-11-12 20:31:21 +0100

    👔 Add check for zero or negative values

--- js/displayKindergeld.js
+++ js/displayKindergeld.js
@@ -7,9 +7,12 @@ function displayKindergeld () { // eslint-disable-line require-jsdoc
     let eMaandloon = document.getElementById('maandloon');
     let eOutput = document.getElementById('output');
 
-    let aantalKinderen = eKinderen.value; // eslint-disable-line no-unused-vars
-    let maandloon = eMaandloon.value; // eslint-disable-line no-unused-vars
+    let aantalKinderen = eKinderen.value;
+    let maandloon = eMaandloon.value;
     let outputText = 'foo';
 
+    if (aantalKinderen <= 0 || maandloon <= 0) {
+        alert('Aantal kinderen en/of maandloon moeten meer dan 0 zijn'); // eslint-disable-line no-alert
+    }
     eOutput.innerHTML = outputText;
 }
