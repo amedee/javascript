f92a47bf62c004b90a25fd96d367c0306365f31b
Amedee Van Gasse 🦄🌈✨ (they/them) <amedee-github@amedee.be>
2021-11-12 18:07:25 +0100

    ♻️ Extract methods for toeslagen

--- js/kindergeld.js
+++ js/kindergeld.js
@@ -2,15 +2,22 @@ const defaultKindergeld = 25;
 const toeslag3deKind = 12.5;
 const toeslag5deKind = 7.5;
 
+let defaultUitkering = aantalKinderen =>
+    defaultKindergeld * aantalKinderen;
+let uitkeringVanafDerdeKind = aantalKinderen =>
+    toeslag3deKind * Math.max(aantalKinderen - 2, 0);
+let uitkeringVanafVijfdeKind = aantalKinderen =>
+    toeslag5deKind * Math.max(aantalKinderen - 4, 0);
+
 /**
  * @param {number} aantalKinderen Hoeveel kinderen heeft de moeder ten laste
  * @param {number} maandloon Wat is het maandloon van de moeder
  * @returns {number} Het kindergeld waar de moeder recht op heeft
  */
 function berekenKindergeld (aantalKinderen, maandloon) { // eslint-disable-line no-unused-vars
-    return (defaultKindergeld * aantalKinderen) +
-        (toeslag3deKind * Math.max(aantalKinderen - 2, 0)) +
-        (toeslag5deKind * Math.max(aantalKinderen - 4, 0));
+    return defaultUitkering(aantalKinderen) +
+        uitkeringVanafDerdeKind(aantalKinderen) +
+        uitkeringVanafVijfdeKind(aantalKinderen);
 }
 
 module.exports = {berekenKindergeld}
