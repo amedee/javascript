30c8cbc10ba5fc5800a6a13b391abc00e47907b5
Amedee Van Gasse 🦄🌈✨ (they/them) <amedee-github@amedee.be>
2021-11-12 21:32:08 +0100

    🚧 Display the calculated amount

--- js/berekenKindergeld.js
+++ js/berekenKindergeld.js
@@ -1,3 +1,34 @@
+window.onload = function () {
+    document.getElementById('deKnop').onclick = displayKindergeld;
+};
+
+function displayKindergeld () { // eslint-disable-line require-jsdoc
+    let eKinderen = document.getElementById('kinderen');
+    let eMaandloon = document.getElementById('maandloon');
+    let eOutput = document.getElementById('output');
+
+    let aantalKinderen = eKinderen.value;
+    let maandloon = eMaandloon.value;
+    let outputText = '';
+
+    if (aantalKinderen <= 0 || maandloon <= 0) {
+        alert('Aantal kinderen en/of maandloon moeten meer dan 0 zijn'); // eslint-disable-line no-alert
+    } else {
+        let kindergeld = berekenKindergeld(aantalKinderen, maandloon);
+        let kindString = 'kind';
+        if (aantalKinderen > 1) {
+            kindString = 'kinderen';
+        }
+
+        outputText = `
+            Met een maandloon van ${maandloon}
+            en ${aantalKinderen} ${kindString},
+            heeft deze moeder recht op €${kindergeld} kindergeld.
+        `
+    }
+    eOutput.innerHTML = outputText;
+}
+
 const defaultKindergeld = 25;
 const toeslag3deKind = 12.5;
 const toeslag5deKind = 7.5;
@@ -34,10 +65,9 @@ let round = num =>
  * @param {number} maandloon Wat is het maandloon van de moeder
  * @returns {number} Het kindergeld waar de moeder recht op heeft
  */
-let berekenKindergeld = (aantalKinderen, maandloon) =>
-    round(Math.max(
+function berekenKindergeld (aantalKinderen, maandloon) {
+    return round(Math.max(
         basisKindergeld(aantalKinderen) * toeslagFactorLoon(maandloon),
         defaultUitkering(aantalKinderen)
     ));
-
-module.exports = {berekenKindergeld}
+}
